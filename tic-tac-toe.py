79fe559f597064adc639621b699fad3f290ccf6b
mrstevenaweiss <weiss.steven@gmail.com>
2018-10-06 14:41:24 -0400

    added new pseudo to tic tac toe

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,15 +1,48 @@
 
 theBoard = {
-    'top-L': ' ', 'top-M': ' ', 'top-R': ' ',
-    'mid-L': ' ', 'mid-M': ' ', 'mid-R': ' ',
-    'bottom-L': ' ', 'bottom-M': ' ', 'bottom-R': ' '
+    '1': ' ', '2': ' ', '3': ' ',
+    '4': ' ', '5': ' ', '6': ' ',
+    '7': ' ', '8': ' ', '9': ' '
     }
 
-def printBoard(board):
-    print(board['top-L'] + '|' + board['top-M'] + '|' + board['top-R'])
-    print('-+-+-')
-    print(board['mid-L'] + '|' + board['mid-M'] + '|' + board['mid-R'])
-    print('-+-+-')
-    print(board['bottom-L'] + '|' + board['bottom-M'] + '|' + board['bottom-R'])
 
-printBoard(theBoard)
+winningCombos = [(1, 2, 3),(4,5,6),(7,8,9),(1,4,7),(2,5,8),(3,6,9),(1,5,9),(3,5,7)]
+
+# def iCanWin:
+# def opponentCanWin:
+
+def printBoard(board):
+    print(board['1'] + '|' + board['2'] + '|' + board['3'])
+    print('-+-+-')
+    print(board['4'] + '|' + board['5'] + '|' + board['6'])
+    print('-+-+-')
+    print(board['7'] + '|' + board['8'] + '|' + board['9'])
+
+
+def tileChecker(move):
+    return theBoard[move] == ' '
+
+def main():
+    turn = 'X'
+    for i in range(9):
+        printBoard(theBoard)
+        print("Turn for: " + turn + ". Move on which space? (use number 1-9)" )
+        move = input()
+
+        if tileChecker(move):
+            print('also working')
+        else:
+            print('Choose another tile' '\n')
+
+
+
+        theBoard[move] = turn
+        if turn == 'X':
+            turn = 'O'
+        else:
+            turn = 'X'
+    print('The Game is Over')
+
+main()
+
+# def endGame()
